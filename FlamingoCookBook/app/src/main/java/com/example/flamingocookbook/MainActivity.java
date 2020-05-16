0c5a0bbb69f27699aec4b9b23559e47ce7888e3f
Phurba Yangji Sherpa <np03a180003@heraldcollege.edu.np>
2020-05-16 19:38:33 +0545

    Signup with authentication

--- FlamingoCookBook/app/src/main/java/com/example/flamingocookbook/MainActivity.java
+++ FlamingoCookBook/app/src/main/java/com/example/flamingocookbook/MainActivity.java
@@ -1,38 +1,76 @@
 package com.example.flamingocookbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText loginUser, loginPassword;
+    private EditText login_email, login_password;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginUser = findViewById(R.id.username);
-        loginPassword = findViewById(R.id.password);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        login_email = findViewById(R.id.login_email_id);
+        login_password = findViewById(R.id.login_password_id);
     }
 
     public void login(View view) {
         //authenticate and login
-        String name = loginUser.getText().toString();
-        String password = loginPassword.getText().toString();
+        String email = login_email.getText().toString().trim();
+        String password = login_password.getText().toString().trim();
 
-        if (name.equals("admin") && password.equals("admin")){
-            
-            startActivity(new Intent(this,HomeActivity.class));
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Email Field Empty", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Password Field Empty", Toast.LENGTH_SHORT).show();
         }
         else{
-            Toast.makeText(this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+            loginUser(email,password);
         }
+
+
+    }
+
+    private void loginUser(String email, String password) {
+        //firebase login code
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                        } else {
+                            Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    public void Signin(View view) {
+        startActivity(new Intent(this,SigninActivity.class));
 
     }
 }
